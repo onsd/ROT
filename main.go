64ed6bebbcf4030d4a0e4cb8b1cfc8402e29b060
Taka <taka@Taka-MacBookPro.local>
2018-09-25 21:32:37 +0900

    add decode func

--- main.go
+++ main.go
@@ -1,51 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"unicode"
-	"strings"
-	"bytes"
-	"os"
-	"flag"
 	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
 )
 
-const upperCase string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowerCase string = "abcdefghijklmnopqrstuvwxyz"
 
+func swap(A,B string)(string,string){
+	return B,A
+}
+func Rot(N int, plain string, flag *bool) (string, error) {
+	var (
+		upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		lowerCase  = "abcdefghijklmnopqrstuvwxyz"
+		rotedUpper = upperCase[N:] + upperCase[:N]
+		rotedLower = lowerCase[N:] + lowerCase[:N]
+	)
 
+	if *flag {
+		swap(upperCase,rotedUpper)
+		swap(lowerCase,rotedLower)
+	}
 
-func Rot(N int,plain string) ( string, error){
-	 rotedUpper := upperCase[N:] + upperCase[:N]
-	 rotedLower := lowerCase[N:] + lowerCase[:N]
-	 var roted bytes.Buffer
-	 for _,v := range plain {
-		 if unicode.IsLower(v) {
-			 index := strings.Index(lowerCase, string(v))
-			 roted.WriteString(string(rotedLower[index]))
-		 }else if unicode.IsUpper(v){
-		 	index := strings.Index(upperCase,string(v))
-		 	roted.WriteString(string(rotedUpper[index]))
-		 }else{
-		 	roted.WriteString(string(v))
-		 }
+	var roted bytes.Buffer
+	for _, v := range plain {
+		if unicode.IsLower(v) {
+			index := strings.Index(lowerCase, string(v))
+			roted.WriteString(string(rotedLower[index]))
+		} else if unicode.IsUpper(v) {
+			index := strings.Index(upperCase, string(v))
+			roted.WriteString(string(rotedUpper[index]))
+		} else {
+			roted.WriteString(string(v))
+		}
 
-	 }
+	}
 
-	 return roted.String() , nil
+	return roted.String(), nil
 }
 
-
-func main(){
+func main() {
 	var (
-		intOpt  = flag.Int("n", 13, "help message for i option")
+		intOpt     = flag.Int("n", 13, "help message for i option")
+		decodeFlag = flag.Bool("d", false, "help message for d option")
 	)
 	flag.Parse()
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	target := scanner.Text()
-	new, _  := Rot(*intOpt,target)
+	new, _ := Rot(*intOpt, target, decodeFlag)
 	fmt.Println(new)
 
 }
-
